1becccc9d1007796ff2ba91d08deac7659525694
azzizulhakim810 <azzizul.hakim202@gmail.com>
2023-09-16 23:54:48 +0600

    setting up all the packages

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Home from "./components/Home/Home"
+
+function App() {
+
+
+  return (
+    <>
+      <div className="bg-[#F3F3F3]">
+
+        <Home></Home>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
