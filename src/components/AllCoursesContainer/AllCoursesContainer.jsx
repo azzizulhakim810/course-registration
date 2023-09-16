953ce86a453ee7e613a6e25b37b5ad9ac0829cab
azzizulhakim810 <azzizul.hakim202@gmail.com>
2023-09-16 23:59:44 +0600

    add all courses component

---
+++ src/components/AllCoursesContainer/AllCoursesContainer.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import SingleCourse from '../SingleCourse/SingleCourse';
+
+const AllCoursesContainer = ({handleSelectedCourse}) => {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    fetch('../../../public/fakeData.json')
+    .then((res) => res.json())
+    .then((data) => setCourses(data))
+  },[])
+
+  return (
+    <div>
+      <div className='grid grid-cols-3 gap-5'>
+      {
+        courses.map(course => <SingleCourse 
+          key={course.id}
+          course = {course}
+          handleSelectedCourse = {handleSelectedCourse}
+          >
+          </SingleCourse>)
+      }
+      </div>
+    </div>
+  );
+};
+
+AllCoursesContainer.propTypes = {
+  handleSelectedCourse: PropTypes.func.isRequired,
+};
+
+export default AllCoursesContainer;
